56f8796c95ad24ad5d8b6ff9e9dbefe5aa00b9a2
Kamilavrhcp <kamilavrhcp@hotmail.com>
2020-11-26 19:11:55 -0300

    exercicio de media

---
+++ media.js
@@ -0,0 +1,8 @@
+//Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array; //
+
+let numberOne = 3;
+let numberTwo = 2;
+let media = 0;
+
+media = (numberOne + numberTwo)/2 ;
+console.log(media);
