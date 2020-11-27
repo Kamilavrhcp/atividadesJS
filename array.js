e6f66c0743abfe9ddad326d9804e2d2667c2d7d9
Kamilavrhcp <kamilavrhcp@hotmail.com>
2020-11-26 23:10:31 -0300

    atualizando

---
+++ array.js
@@ -0,0 +1,13 @@
+//Utilizando for , descubra qual o maior valor contido no array e imprima-o;
+
+let numbers = [72, 5, 6, 10, 18];
+
+let min =[] 
+
+
+
+for(let index=0; index < 6; index += 1){
+min = Math.max(... numbers);
+}
+
+  console.log(min);
