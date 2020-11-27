0cc6db05f011f8b20265b23300cb5d65d8e35262
Kamilavrhcp <kamilavrhcp@hotmail.com>
2020-11-27 15:19:24 -0300

    maior numero

--- array.js
+++ array.js
@@ -1,29 +1,23 @@
 //Utilizando for , descubra qual o maior valor contido no array e imprima-o;
 
-/*let numbers = [72, 5, 6, 10, 18];
-
-let min =[] 
-
-
+let numbers = [72, 5, 6, 10, 18];
+let max =[] 
 
 for(let index=0; index < 6; index += 1){
-min = Math.max(... numbers);
+max = Math.max(... numbers);
 }
 
-  console.log(min); */
+  console.log(max); 
+  
 
-  /*
-  Utilizando o array abaixo, percorra-o somando todos os valores. Caso o valor final seja maior que 15, imprima-o. Caso seja igual ou menor que 15, imprima a mensagem: "valor menor que 16": */
+//Utilizando for , descubra qual o menor valor contido no array e imprima-o;
 
-  let fruits = [3, 4, 0, 1, 2];
-  let valor = 0;
 
-  for(let index=0; index < fruits.length; index +=1){
-    valor += fruits[index];
+  let numbers = [72, 5, 6, 10, 18];
+  let min =[] 
+
+  for(let index=0; index < 6; index += 1){
+  min = Math.min(... numbers);
   }
 
-  if(valor > 15){
-    console.log ("Valor maior que 15");
-  } else{
-    console.log ("Valor menor que 15")
-  }
+  console.log(min); 
