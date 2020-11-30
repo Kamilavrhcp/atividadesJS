fa0b1040177f5c8d43beda3a68e2c56e3597a9ee
Kamilavrhcp <kamilavrhcp@hotmail.com>
2020-11-30 14:57:06 -0300

    Atualizacoes

---
+++ menorValor.js
@@ -0,0 +1,10 @@
+//Utilizando for , descubra qual o menor valor contido no array e imprima-o
+
+let numbers = [72, 5, 6, 10, 18];
+let min =[] 
+
+for(let index=0; index < 6; index += 1){
+min = Math.min(... numbers);
+}
+
+console.log(min); 
