696e23f36e6a690366fb06f62d85d02b9b299b53
Kamilavrhcp <kamilavrhcp@hotmail.com>
2020-11-30 13:25:29 -0300

    Atualizaçoes

--- array.js
+++ array.js
@@ -8,16 +8,3 @@ max = Math.max(... numbers);
 }
 
   console.log(max); 
-  
-
-//Utilizando for , descubra qual o menor valor contido no array e imprima-o;
-
-
-  let numbers = [72, 5, 6, 10, 18];
-  let min =[] 
-
-  for(let index=0; index < 6; index += 1){
-  min = Math.min(... numbers);
-  }
-
-  console.log(min); 
