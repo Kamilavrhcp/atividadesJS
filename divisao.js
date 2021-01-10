a25d6c5f0cdd56224cbb25972bff7e81915e7c4a
Kamilavrhcp <kamilavrhcp@hotmail.com>
2021-01-10 19:07:56 -0300

    lesson: atualizações

---
+++ divisao.js
@@ -0,0 +1,10 @@
+//Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2 .
+
+let numbers = [];
+
+for(let index=0; index < 26; index +=1) {
+numbers.push(index)
+
+console.log(numbers[index] / 2);
+
+}
